13da5be667ba3b626535325dd117aa69f224a3b1
Ирина <1r04k@mail.ru>
2023-07-26 17:36:59 +0300

    add signals.py

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+from PIL import Image
 
 
 @receiver(post_save, sender=User)
@@ -13,3 +14,9 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+    img = Image.open(instance.profile.image.path)
+    if img.height > 300 or img.width > 300:
+        output_size = (300, 300)
+        img.thumbnail(output_size)
+        img.save(instance.profile.image.path)
